ea5eaa0c24c85e15dab637f1efc183a505eaf76b
ranjeet kumar <ranjeetkumarkanaily@gmail.com>
2016-05-31 09:11:21 +0530

    Every Lesson is divided into multiple contents(parts). Relationship between Lesson and Content added.

--- db/migrate/20160531033453_create_contents.rb
+++ db/migrate/20160531033453_create_contents.rb
@@ -2,6 +2,7 @@ class CreateContents < ActiveRecord::Migration
   def change
     create_table :contents do |t|
       t.string :name
+      t.belongs_to :lesson
 
       t.timestamps null: false
     end

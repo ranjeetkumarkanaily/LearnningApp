b8448a4d41138a129aaebd4813a76d3aa1401af9
ranjeet kumar <ranjeetkumarkanaily@gmail.com>
2016-05-31 10:27:50 +0530

    Teacher can also check the progress of his students.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,13 @@
 class Teacher < ActiveRecord::Base
 	has_many :students
+
+	def studentsProgress
+		progressCard = {}
+
+		students.each do |s|
+			progressCard[s.name] = s.progress
+		end
+
+		progressCard
+	end
 end
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe Teacher, type: :model do
 
   	expect(Teacher.count).to eq(1)
   end
+
+  it "should display all students progress" do
+  	l = Lesson.create!(name: "Lesson 1")
+  	c1 = l.contents.create!(name: "Content 1")
+  	c2 = l.contents.create!(name: "Content 2")
+  	c3 = l.contents.create!(name: "Content 3")
+  	c4 = l.contents.create!(name: "Content 4")
+
+  	t = Teacher.create!(name: "Teacher 1")
+  	s = t.students.create!(name: "Student 1")
+
+  	CompletedLesson.create!(lesson_id: l.id, content_id: c1.id, student_id: s.id)
+  	CompletedLesson.create!(lesson_id: l.id, content_id: c2.id, student_id: s.id)
+  	CompletedLesson.create!(lesson_id: l.id, content_id: c3.id, student_id: s.id)
+  	CompletedLesson.create!(lesson_id: l.id, content_id: c4.id, student_id: s.id)
+
+  	studentPorgressCard = {"#{s.name}"=>["L#{l.id} P1", "L#{l.id} P2", "L#{l.id} P3", "L#{l.id} P4"]}
+
+  	expect(t.studentsProgress).to eq(studentPorgressCard)
+  end
 end

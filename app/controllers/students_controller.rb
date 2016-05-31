c8e2cd635ad3f606534df405a988b949b98b1dd2
ranjeet kumar <ranjeetkumarkanaily@gmail.com>
2016-05-31 11:37:28 +0530

    Refactored students controller for resulting format

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
   def progress
+  	student = Student.find(params[:id])
+  	@student_progress = student.progress
+  	result_hash = {}
+  	result_hash[student.name] = @student_progress
+
+  	render json:  result_hash
   end
 
   def edit
-  	puts params.inspect
+  	
+  end
+
+  def setProgress
+  	content = Content.where(symbolic_name: params[:lesson])[0]
+  	student = Student.find(params[:id])
+  	student.setProgress content.lecture_id, content.id
   end
 end

dd1f89687334e8557a9848bae5fb8fbf80ac663a
ranjeet kumar <ranjeetkumarkanaily@gmail.com>
2016-05-31 09:14:45 +0530

    Teacher has multiple students. Relationship between Teacher and Student added.

--- db/migrate/20160531034258_create_students.rb
+++ db/migrate/20160531034258_create_students.rb
@@ -2,6 +2,7 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :name
+      t.belongs_to :teacher
 
       t.timestamps null: false
     end

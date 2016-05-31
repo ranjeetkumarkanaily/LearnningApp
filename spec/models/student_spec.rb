e37221e062b273e1044539b17d26a03ea13b1d46
ranjeet kumar <ranjeetkumarkanaily@gmail.com>
2016-05-31 10:02:43 +0530

    Refactored techer model spec

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Student, type: :model do
   it "should create one Student" do
-  	l = Student.create!(name: "Student 1")
+  	Student.create!(name: "Student 1")
 
   	expect(Student.count).to eq(1)
+  end
+
+  it "should check the progress of a student" do
+
   end
 end
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
-  it "should create one Lesson" do
-  	l = Lesson.create!(name: "Lesson 1")
+  it "should create one Teacher" do
+  	Teacher.create!(name: "Teacher 1")
 
-  	expect(Lesson.count).to eq(1)
+  	expect(Teacher.count).to eq(1)
   end
 end
